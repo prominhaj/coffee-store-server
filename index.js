be871838b72ee3976e9128512a7495a32afbff20
Md Minhaj <parsonal322532@gmail.com>
2024-01-15 04:47:00 +0600

    change mongo db connect and remove await

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect();
 
         const database = client.db('coffee-storesDB').collection('coffee-stores')
 
